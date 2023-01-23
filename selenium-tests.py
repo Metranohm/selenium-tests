8b9e530dfd52361ad2e25220e6809152337de0d2
Metranohm <andrewwink@gmail.com>
2023-01-23 16:14:36 -0600

    update path for chromedriver

--- selenium-tests.py
+++ selenium-tests.py
@@ -1,8 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-PATH = "home/metranohm/code/chromedriver"
+PATH = "/usr/bin/chromedriver"
 driver = webdriver.Chrome(PATH)
 
 driver.get("https://techwithtim.net")
 print(driver.title)
+
+search = driver.find_element("name", "s")
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+main = driver.find_element("id", "main")
+print(main.text)
+
+
 driver.quit()
