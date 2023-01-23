44db5085ac976464def03d11ae0349554060c377
Metranohm <andrewwink@gmail.com>
2023-01-23 15:38:30 -0600

    first test

--- selenium-tests.py
+++ selenium-tests.py
@@ -1,1 +1,8 @@
-import selenium
+from selenium import webdriver
+
+PATH = "home/metranohm/code/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://techwithtim.net")
+print(driver.title)
+driver.quit()
