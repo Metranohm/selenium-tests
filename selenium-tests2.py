323e20af31b00988d61eefb3001318cd4cefe784
Metranohm <andrewwink@gmail.com>
2023-01-24 16:10:01 -0600

    update portfolio test

--- selenium-tests2.py
+++ selenium-tests2.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
-# Navigate to the website
+# Navigate to the portfolio website
 driver.get("https://andrew-winkler-portfolio.netlify.app/")
 
-# Get all the links on the page
-links = driver.find_elements(By.TAG_NAME, "a")
+# Find all project links on the page
+project_links = driver.find_elements_by_css_selector("a.project-link")
 
-# Visit each link
-for link in links:
-    href = link.get_attribute("href")
-    driver.get(href)
+# Iterate through each project link and open it
+for link in project_links:
+    link.click()
+    driver.back()
 
-# Close the browser
+# Close the browser window
 driver.quit()
