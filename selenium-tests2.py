ff26095bcb336a7021e76b6519002ac0039f727a
Metranohm <andrewwink@gmail.com>
2023-01-24 16:02:34 -0600

    portfolio link test

--- selenium-tests2.py
+++ selenium-tests2.py
@@ -1,43 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-import time
 
-PATH = "/usr/bin/chromedriver"
-s=Service(ChromeDriverManager().install())
-driver = webdriver.Chrome(service=s)
+# Create a new instance of the Firefox driver
+driver = webdriver.Firefox()
 
-driver.get("https://techwithtim.net")
+# Navigate to the website
+driver.get("https://andrew-winkler-portfolio.netlify.app/")
 
-link = driver.find_element((By.LINK_TEXT, "Python Programming"))
-link.click()
+# Get all the links on the page
+links = driver.find_elements(By.TAG_NAME, "a")
 
-try: 
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
-    )
-    element.click()
-except:
-    driver.quit()
+# Visit each link
+for link in links:
+    href = link.get_attribute("href")
+    driver.get(href)
 
-print(driver.title)
-
-search = driver.find_element("name", "s")
-search.send_keys("test")
-search.send_keys(Keys.RETURN)
-
-try: 
-  main = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, "main"))
-    )
-  articles = main.find_elements(By.TAG_NAME, "article")
-for article in articles:
-header = article.find_element(By.CLASS_NAME, "entry-summary")
-print(header.text)
-    
- finally:
-     driver.quit()
+# Close the browser
+driver.quit()
