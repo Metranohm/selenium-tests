d87ffbf2f0b316b03e9b24780da6858b42b30056
Metranohm <andrewwink@gmail.com>
2023-01-27 15:05:11 -0600

    headless browser test 2

--- selenium-tests3.py
+++ selenium-tests3.py
@@ -1,21 +1,12 @@
 from selenium import webdriver
-
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-
-# set up webdriver
-driver = webdriver.Chrome(chrome_options=options)
-
-# navigate to Google
-driver.get("https://www.google.com")
-
-# find search box and enter search term
-search_box = driver.find_element_by_name("q")
-search_box.send_keys("andrew winkler")
-search_box.submit()
-
-# assert that the first result has the search term in the title
-assert "andrew winkler" in driver.title
-
-# close browser
-driver.quit()
+#browser exposes an executable file
+#Through Selenium test we will invoke the executable file which will then #invoke actual browser
+driver = webdriver.Chrome(executable_path="usr/local/bin/chromedriver")
+# to maximize the browser window
+driver.maximize_window()
+#get method to launch the URL
+driver.get("https://www.tutorialspoint.com/index.htm")
+#to refresh the browser
+driver.refresh()
+#to close the browser
+driver.close()
