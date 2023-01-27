3429a08973cb0405692c9deac08dc04591b3e8bf
Metranohm <andrewwink@gmail.com>
2023-01-27 15:14:20 -0600

    update chrome test

--- selenium-tests3.py
+++ selenium-tests3.py
@@ -1,12 +1,19 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 #browser exposes an executable file
 #Through Selenium test we will invoke the executable file which will then #invoke actual browser
 driver = webdriver.Chrome(executable_path="usr/local/bin/chromedriver")
-# to maximize the browser window
-driver.maximize_window()
-#get method to launch the URL
-driver.get("https://www.tutorialspoint.com/index.htm")
-#to refresh the browser
-driver.refresh()
-#to close the browser
-driver.close()
+
+# navigate to Google
+driver.get("https://www.google.com")
+
+# find search box and enter search term
+search_box = driver.find_element(By.NAME, "q")
+search_box.send_keys("andrew winkler")
+search_box.submit()
+
+# assert that the first result has the search term in the title
+assert "andrew winkler" in driver.title
+
+# close browser
+driver.quit()
