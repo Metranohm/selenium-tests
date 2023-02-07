65ba1564b7356add0ddeb386f4f8464d088c1c4d
Metranohm <andrewwink@gmail.com>
2023-02-07 10:57:49 -0600

    update test

--- selenium-tests8.py
+++ selenium-tests8.py
@@ -1,10 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+import time
 
-driver = webdriver.Chrome(executable_path="usr/local/bin/chromedriver")
+s = Service('usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=s)
+
 driver.get("https://orteil.dashnet.org/cookieclicker/")
 
-driver.implicitly_wait(5)
+time.sleep(10)
 
-cookie = driver.find_element((By.ID), "bigCookie")
+cookie = driver.find_element(By.ID, "bigCookie")
+cookie_count = driver.find_element(By.ID, "cookies")
+items = [driver.find_element(By.ID, "productPrice" + str(i)) for i in range(1, -1, -1)]
+
+actions = ActionChains(driver)
+actions.click(cookie)
+
+for i in range (5000):
+    actions.perform()
+    count = cookie_count.text
+    print(count)
