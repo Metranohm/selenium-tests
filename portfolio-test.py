ef82dabd121f432fb04020a862bb92a5971181ae
Metranohm <andrewwink@gmail.com>
2023-02-18 16:02:11 -0600

    debug portfolio test

--- portfolio-test.py
+++ portfolio-test.py
@@ -7,3 +7,17 @@ import time
 
 s = Service('usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=s)
+driver.get('https://andrew-winkler-portfolio.netlify.app/')
+
+# Find all the elements in the DOM
+elements = driver.find_elements(By.CSS_SELECTOR('*'))
+
+# Check that each element is present on the page and visible
+for element in elements:
+    try:
+        assert element.is_displayed()
+    except AssertionError:
+        print(f"Element not visible: {element.tag_name} {element.get_attribute('class')}")
+
+# Close the browser
+driver.quit()
