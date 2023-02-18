88751e5632ce723354627f93f589d05af0286b85
Metranohm <andrewwink@gmail.com>
2023-02-18 16:12:37 -0600

    test all links on portfolio

--- portfolio-test.py
+++ portfolio-test.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(service=s)
 driver.get('https://andrew-winkler-portfolio.netlify.app/')
 
 # Find all the elements in the DOM
-elements = driver.find_elements(By.CSS_SELECTOR('*'))
+elements = driver.find_elements(By.CSS_SELECTOR, "*")
 
 # Check that each element is present on the page and visible
 for element in elements:
@@ -18,6 +18,19 @@ for element in elements:
         assert element.is_displayed()
     except AssertionError:
         print(f"Element not visible: {element.tag_name} {element.get_attribute('class')}")
+        
+        
+links = driver.find_elements(By.TAG_NAME, 'a')
+
+# Check that each link is working
+for link in links:
+    url = link.get_attribute('href')
+    if url is not None:
+        driver.get(url)
+        if driver.title == "404 Page Not Found" or "not found" in driver.page_source:
+            print(f"Broken link: {url}")
+        else:
+            print(f"Valid link: {url}")
 
 # Close the browser
 driver.quit()
