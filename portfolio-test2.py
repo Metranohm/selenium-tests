d2b487d0a7edde7a17339e7a8818bfa3941083ca
Metranohm <andrewwink@gmail.com>
2023-02-21 15:14:49 -0600

    imports and initialize test

---
+++ portfolio-test2.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+import time
+
+s = Service('usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=s)
+driver.get('https://andrew-winkler-portfolio.netlify.app/')
