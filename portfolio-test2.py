89454a329c435fb7feac55e4a4cd2a0b4bb1082e
Metranohm <andrewwink@gmail.com>
2023-02-21 15:34:11 -0600

    update test

--- portfolio-test2.py
+++ portfolio-test2.py
@@ -1,16 +1,29 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 import time
 
 s = Service('usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=s)
-driver.get('https://andrew-winkler-portfolio.netlify.app/')
+browser = webdriver.Chrome()
 
-about = driver.find_element(By.LINK_TEXT, "About")
+# navigate to the website
+driver.get("https://andrew-winkler-portfolio.netlify.app/")
 
-about.click()
+# get all the links in the navigation bar
+nav_links = browser.find_elements(By.CSS_SELECTOR, "NavBar_name__ZPtL0")
 
-time.sleep(100)
+# iterate over each link and click it
+for link in nav_links:
+    link.click()
+    
+    # wait for the new page to load
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, "h1")))
+    
+    # go back to the home page
+    driver.back()
+
+# close the browser
+driver.quit()
