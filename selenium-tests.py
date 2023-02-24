bd4c1cd2ec186c5b43af8db58cf0675265b1563c
Metranohm <andrewwink@gmail.com>
2023-02-24 17:46:31 -0600

    update booking test

--- selenium-tests.py
+++ selenium-tests.py
@@ -1,43 +1,44 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
-PATH = "/usr/bin/chromedriver"
-s=Service(ChromeDriverManager().install())
+s = Service('usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=s)
+# Random.org testing
+driver.get("https://www.random.org/integers")
 
-driver.get("https://techwithtim.net")
+num_numbers = driver.find_element(By.CSS_SELECTOR, 'input[name="num"]')
+num_numbers.clear()
+num_numbers.send_keys("10")
+submit_button = driver.find_element(By.CSS_SELECTOR, 'input[value="Get Numbers"]')
+submit_button.click()
+time.sleep(3)
+driver.back()
+time.sleep(3)
+num_numbers = driver.find_element(By.CSS_SELECTOR, 'input[name="num"]')
+num_numbers.clear()
+num_numbers.send_keys("1000")
+time.sleep(3)
+submit_button = driver.find_element(By.CSS_SELECTOR, 'input[value="Get Numbers"]')
+submit_button.click()
+time.sleep(3)
+driver.back()
+# PHP Travels testing
+driver.get('https://phptravels.com/demo/')
+time.sleep(1)
+first_name = driver.find_element(By.CSS_SELECTOR, 'input[class="first_name input mb1"]')
+first_name.send_keys("Andrew")
+last_name = driver.find_element(By.CSS_SELECTOR, 'input[name="last_name"]')
+last_name.send_keys("Winkler")
+business_name = driver.find_element(By.CSS_SELECTOR, 'input[name="business_name"]')
+business_name.send_keys("Winkler Enterprises")
+email_address = driver.find_element(By.CSS_SELECTOR, 'input[name="email"]')
+email_address.send_keys('andrewwink@gmail.com')
+time.sleep(10)
+submit2_button = driver.find_element(By.CSS_SELECTOR, 'button[id="demo"]')
+submit2_button.click()
+time.sleep(7)
+driver.quit()
 
-link = driver.find_element((By.LINK_TEXT, "Python Programming"))
-link.click()
-
-try: 
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
-    )
-    element.click()
-except:
-    driver.quit()
-# print(driver.title)
-
-# search = driver.find_element("name", "s")
-# search.send_keys("test")
-# search.send_keys(Keys.RETURN)
-
-# try: 
-#     main = WebDriverWait(driver, 10).until(
-#         EC.presence_of_element_located((By.ID, "main"))
-#     )
-    
-#     articles = main.find_elements(By.TAG_NAME, "article")
-#     for article in articles:
-#         header = article.find_element(By.CLASS_NAME, "entry-summary")
-#         print(header.text)
-    
-# finally:
-#     driver.quit()
