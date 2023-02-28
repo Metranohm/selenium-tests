9af48f54f3420ed21b9c24064f6000dd488fac5c
Metranohm <andrewwink@gmail.com>
2023-02-28 17:29:47 -0600

    clean code

--- phptravels-test.py
+++ phptravels-test.py
@@ -11,56 +11,77 @@ driver = webdriver.Chrome(service=s)
 # PHP Travels testing
 driver.get('https://phptravels.com/demo/')
 driver.maximize_window()
+
 time.sleep(1)
+
 pricing_button = driver.find_element(By.CSS_SELECTOR, 'a[class="jfHeader-menuListLink jfHeader-dynamicLink js-tracking js-myforms"')
 pricing_button.click()
 time.sleep(1)
+
 driver.back()
 time.sleep(1)
+
 features_dropdown = driver.find_element(By.CSS_SELECTOR, 'a[class="jfHeader-menuListLink jfHeader-dynamicLink js-tracking"]')
 features_dropdown.click()
+
 time.sleep(1)
+
 integrations_dropdown = driver.find_element(By.CSS_SELECTOR, 'a[class="jfHeader-menuListLink jfHeader-dynamicLink js-tracking js-integrations"]')
 integrations_dropdown.click()
+
 time.sleep(1)
+
 company_dropdown = driver.find_element(By.CSS_SELECTOR, 'a[data-text-name="support"]')
 company_dropdown.click()
+
 time.sleep(1)
 
 # test inputs
 first_name1 = driver.find_element(By.CSS_SELECTOR, 'input[class="first_name input mb1"]')
 first_name1.send_keys("Halcyon")
+
 last_name1 = driver.find_element(By.CSS_SELECTOR, 'input[name="last_name"]')
 last_name1.send_keys("&on&on")
+
 business_name = driver.find_element(By.CSS_SELECTOR, 'input[name="business_name"]')
 business_name.send_keys("Orbital Enterprises")
+
 email_address = driver.find_element(By.CSS_SELECTOR, 'input[name="email"]')
 email_address.send_keys('turdman@fire.com')
+
 time.sleep(7)
+
 submit2_button = driver.find_element(By.CSS_SELECTOR, 'button[id="demo"]')
 submit2_button.click()
-time.sleep(3)
-# signup_button = driver.find_element(By.CSS_SELECTOR, 'a[class="jfHeader-menuListLink jfHeader-dynamicLink jfHeader-signup-action signup  btn-outline-dark"]')
-# time.sleep(3)
 
+time.sleep(3)
 
 # test signup: https://phptravels.org/register.php
 driver.get('https://phptravels.org/register.php')
 driver.maximize_window()
+
 drop=Select(driver.find_element(By.CSS_SELECTOR, 'select[id="inputCountry"]'))
 drop.select_by_visible_text('United States')
+
 time.sleep(2)
+
 input_city = driver.find_element(By.CSS_SELECTOR, 'input[id="inputCity"]')
 input_city.send_keys("Austin")
+
 time.sleep(3)
+
 first_name2 = driver.find_element(By.CSS_SELECTOR, 'input[name="firstname"]')
 first_name2.send_keys("Halcyon")
+
 last_name2 = driver.find_element(By.CSS_SELECTOR, 'input[name="lastname"]')
 last_name2.send_keys("&on&on")
+
 register_email = driver.find_element(By.CSS_SELECTOR, 'input[id="inputEmail"]')
 register_email.send_keys('turdman@fire.com')
+
 input_phone = driver.find_element(By.CSS_SELECTOR, 'input[id="inputPhone"]')
 input_phone.send_keys('512-555-5555')
+
 company_name = driver.find_element(By.CSS_SELECTOR, 'input[id="inputCompanyName"]')
 company_name.send_keys("Orbital Enterprises")
 
@@ -70,18 +91,26 @@ time.sleep(3)
 
 input_postcode = driver.find_element(By.CSS_SELECTOR, 'input[id="inputPostcode"]')
 input_postcode.send_keys('78758')
+
 state_select = Select(driver.find_element(By.CSS_SELECTOR, 'select[id="stateselect"]'))
 state_select.select_by_visible_text('Texas')
+
 time.sleep(2)
+
 input_moblie = driver.find_element(By.CSS_SELECTOR, 'input[name="customfield[2]"]')
 input_moblie.send_keys('1234567890')
+
 input_password1 = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
 input_password1.send_keys('password84$')
+
 input_password2 = driver.find_element(By.CSS_SELECTOR, 'input[name="password2"]')
 input_password2.send_keys('password84$')
+
 time.sleep(10)
+
 register_button = driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
 register_button.click()
+
 time.sleep(10)
 
 
--- portfolio-test.py
+++ portfolio-test.py
@@ -8,29 +8,12 @@ import time
 s = Service('usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=s)
 driver.get('https://andrew-winkler-portfolio.netlify.app/')
+driver.maximize_window()
 
-# Find all the elements in the DOM
-elements = driver.find_elements(By.LINK_TEXT, "About")
-
-# Check that each element is present on the page and visible
-for element in elements:
-    try:
-        assert element.is_displayed()
-    except AssertionError:
-        print(f"Element not visible: {element.tag_name} {element.get_attribute('class')}")
-        
-        
-links = driver.find_elements(By.TAG_NAME, 'a')
-
-# Check that each link is working
-for link in links:
-    url = link.get_attribute('href')
-    if url is not None:
-        driver.get(url)
-        if driver.title == "404 Page Not Found" or "not found" in driver.page_source:
-            print(f"Broken link: {url}")
-        else:
-            print(f"Valid link: {url}")
+about_link = driver.find_element(By.CSS_SELECTOR, 'a[href="/about"]')
+about_link.click()
+about_link.click()
+time.sleep(3)
 
 # Close the browser
 driver.quit()
