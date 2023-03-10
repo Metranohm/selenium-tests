743ba5955b7a417910a441fb1d154607f79a42f6
Metranohm <andrewwink@gmail.com>
2023-03-10 12:32:49 -0600

    complete name & password generation

--- phpsignup_bot.py
+++ phpsignup_bot.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+import tkinter as tk
 import time
 
 s = Service('usr/local/bin/chromedriver')
@@ -11,6 +12,8 @@ driver = webdriver.Chrome(service=s)
 # PHP Travels testing
 driver.get('https://www.random-name-generator.com/united-states')
 driver.maximize_window()
+print(driver.title)
+print(driver.current_url)
 
 set_single = driver.find_element(By.XPATH, "//input[@type='number']")
 set_single.clear()
@@ -20,10 +23,24 @@ generate_button = driver.find_element(By.XPATH, "//button[@type='submit']")
 generate_button.click()
 
 time.sleep(1)
-copied_name = driver.find_element(By.XPATH, "//button[@class='btn-copy-name']").text
-print(copied_name)
+
+copyname_button = driver.find_element(By.XPATH, "/html[1]/body[1]/div[1]/div[3]/div[2]/div[1]/div[1]/ul[1]/li[1]/span[1]/button[1]/img[1]").click()
+root = tk.Tk()
+new_name = root.clipboard_get()
+print(new_name)
+
+time.sleep(1)
+
+driver.get('https://andrews-js-password-generator.netlify.app/')
+
+driver.find_element(By.ID, 'length').clear()
+driver.find_element(By.ID, 'length').send_keys('10')
+driver.find_element(By.ID, 'include-numbers').click()
+driver.find_element(By.CSS_SELECTOR, 'button[id="generate-button"]').click()
+copy_button = driver.find_element(By.ID, 'copy-button').click()
+root = tk.Tk()
+new_pw = root.clipboard_get()
+print(new_pw)
 
 
-
-
-time.sleep(300)
+driver.quit() 
