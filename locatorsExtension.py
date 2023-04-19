71031c3f8b73b81ebbe3d7000f96d4e18c949cf6
Metranohm <andrewwink@gmail.com>
2023-04-18 19:24:32 -0500

    assert test

--- locatorsExtension.py
+++ locatorsExtension.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 
+
 s = Service('usr/local/bin/chromedriver')
 driver = webdriver.Chrome(service=s)
 driver.get("https://rahulshettyacademy.com/client")
@@ -10,5 +11,7 @@ driver.get("https://rahulshettyacademy.com/client")
 driver.find_element(By.LINK_TEXT, "Forgot password?").click()
 driver.find_element(By.XPATH, "//form/div[1]/input").send_keys("demo@gmail.com")
 driver.find_element(By.CSS_SELECTOR, "#userPassword").send_keys("hello@1234")
+driver.find_element(By.XPATH, "//form/div[3]/input").send_keys("hello@1234")
+driver.find_element(By.XPATH, "//button[text()='Save New Password']").click()
 
 time.sleep(20)
